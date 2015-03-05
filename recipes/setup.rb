39e5bb669272a1d288455e6d25c41ff15cb44b5c
k2works <kakimomokuri@gmail.com>
2015-03-05 19:28:21 +0900

    Test Kitchenを使う

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,6 +1,13 @@
 # https://github.com/sstephenson/ruby-build/wiki#build-failure-of-fiddle-with-ruby-220
-package "libffi-dev" do
-  action :install
+case node["platform"]
+  when "ubuntu"
+    package "libffi-dev" do
+      action :install
+    end
+  when "centos", "amazon", "fedora"
+    package "libffi-devel" do
+      action :install
+    end
 end
 
 include_recipe 'ruby-env::setup_ruby_rbenv'
